6c49e6f27ad7f59939c0a7da54b9f66f0bc55710
Ayden Janssen <akira.janssen@dfki.de>
2021-09-24 20:23:27 +0200

    Add scrolling

--- applauncher/window.py
+++ applauncher/window.py
@@ -2,7 +2,7 @@ import sys
 import os
 from functools import partial
 
-from PyQt5.QtCore import QSize, Qt, QTimer, QTime
+from PyQt5.QtCore import QSize, Qt, QTimer, QTime, QPoint
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QSplitter,  QLabel, QScrollArea
 from PyQt5.QtGui import QPixmap, QIcon, QFont
 
@@ -12,8 +12,9 @@ class AppLauncherWindow(QMainWindow):
     def __init__(self, width, height):
         super().__init__()
 
+        self.width = width
         self.normal_button = int(height * 0.35)
-        self.small_button = int(height * 0.1)
+        self.small_button = int(height * 0.08)
         self.buttons = 2 * [None]
 
         self.setWindowTitle("App Launcher")
@@ -29,22 +30,37 @@ class AppLauncherWindow(QMainWindow):
         self.main_screen.setLayout(layout)
         self.current_row = 0
         self.last_index = 0
-        # self.main_screen.setStretchFactor(1, 0.2)
 
         self.setCentralWidget(self.main_screen)
-
         self.buttons[0][0].setFocus()
 
     def keyPressEventButton(self, event, row, index):
         key = event.key()
 
         if key == Qt.Key_Left and index > 0:
-            self.buttons[row][index-1].setFocus()
+            new_button = self.buttons[row][index-1]
+            new_button.setFocus()
+            # pos = new_button.pos().x()
+            pos = new_button.mapToGlobal(QPoint(0, 0)).x()
+            print(pos)
+            if pos < 0:
+                scroll_bar = self.scroll.horizontalScrollBar()
+                scroll_bar.setValue(scroll_bar.value() - (self.normal_button + 6))
+
         elif key == Qt.Key_Right and index < len(self.buttons[row]) - 1:
-            self.buttons[row][index+1].setFocus()
+            new_button = self.buttons[row][index+1]
+            new_button.setFocus()
+            # pos = new_button.pos().x()
+            pos = new_button.mapToGlobal(QPoint(0, 0)).x()
+            print(pos)
+            if pos + self.normal_button > self.width:
+                scroll_bar = self.scroll.horizontalScrollBar()
+                scroll_bar.setValue(scroll_bar.value() + (self.normal_button + 6))
+
         elif key == Qt.Key_Up and row == 1:
             self.buttons[row-1][self.last_index].setFocus()
             self.last_index = index
+
         elif key == Qt.Key_Down and row == 0:
             self.buttons[row+1][self.last_index].setFocus()
             self.last_index = index
@@ -102,6 +118,7 @@ class AppLauncherWindow(QMainWindow):
         scroll.setAlignment(Qt.AlignVCenter)
         scroll.horizontalScrollBar().hide()
         scroll.setFixedHeight(self.normal_button + 30)
+        self.scroll = scroll
         return scroll
 
     def settingWindow(self):
