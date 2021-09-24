a32b2dabcdf4b69cd8ff82784dfec15570089963
Ayden Janssen <akira.janssen@dfki.de>
2021-09-24 20:27:28 +0200

    Adjust side margins

--- applauncher/window.py
+++ applauncher/window.py
@@ -91,7 +91,7 @@ class AppLauncherWindow(QMainWindow):
         scroll = QScrollArea()
         window = QWidget()
         layout = QHBoxLayout()
-        layout.setContentsMargins(self.normal_button/2, 0, self.normal_button/2, 0)
+        layout.setContentsMargins(self.normal_button/1.92, 0, self.normal_button/1.92, 0)
 
         apps = [
             ('img/youtube.png', 'firefox -kiosk www.youtube.com/tv'),
